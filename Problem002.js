cbc6f4efb041785522f0d138046df8d078aaa8cc
Ismail Ansari <ismail.ansari@citiustech.com>
2014-01-13 11:30:31 +0530

    Re-solved problem 2

---
+++ Problem002.js
@@ -0,0 +1,19 @@
+var common = require("./Common");
+
+exports.run = function () {
+    var limit = 4000000,
+    	sum = 0;
+
+    var i = 1,
+    	currentTerm = 0,
+    	myFunc = new common.Memoizer(common.fibonacciMemoizable);
+    while(currentTerm<limit) {
+	currentTerm = myFunc.getValue(i);
+	if(currentTerm%2===0)
+	    sum += currentTerm;
+	i++;
+    };
+
+    console.log("sum: " + sum);
+    return sum;
+};
