1e10596e884abb671db2fb9481b0bb60f2047c8d
Ismail Ansari <ismail.ansari@citiustech.com>
2014-01-13 11:43:07 +0530

    Re-solved problem 3

---
+++ Problem003.js
@@ -0,0 +1,15 @@
+var common = require("./Common");
+
+exports.run = function () {
+    var limit = 10000,
+    	number = 600851475143,
+    	primes = common.getPrimesBelowNumber(limit),
+    	output;
+
+    for(var i=primes.length-1; i>0 && !output; i--)
+	if(number%primes[i]===0)
+	    output = primes[i];
+
+    console.log("output: " + output);
+    return output;
+};
