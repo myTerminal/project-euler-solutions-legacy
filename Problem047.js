ee590200e4200f18a748ed2063f95b3404d5ac2b
Ismail Ansari <ismail.ansari@citiustech.com>
2014-05-13 16:09:19 +0530

    Refactored problems 47 & 48

--- Problem047.js
+++ Problem047.js
@@ -5,42 +5,52 @@ exports.run = function () {
         primes = common.getPrimesBelowNumber(50000),
         primesL = [],
         divisors = [],
-        number;
+        number,
+	getPrimeFactors = function (number) {
+	    var primeFactors = [],
+		count;
+
+	    for (var index=0; index<primesL.length; index++) {
+		count = 0;
+
+		while (number%primesL[index] == 0) {
+		    count++;
+		    number = number / primesL[index];
+		}
+
+		if (count > 0)
+		    primeFactors.push(primesL[index]);
+	    }
+
+	    return [primeFactors.toString(), primeFactors.length];
+	};
+
     (function getPrimes() {
-	for(var i=0; i<primes.length; i++)
-	    if(primes[i])
+	for (var i=0; i<primes.length; i++)
+	    if (primes[i])
 		primesL.push(i);
     })();
-
-    function getPrimeFactors(number) {
-	var primeFactors = [],
-	    count;
-	for(var index=0; index<primesL.length; index++) {
-	    count = 0;
-	    while(number%primesL[index] == 0) {
-		count++;
-		number = number/primesL[index];
-	    }
-	    if(count>0)
-		primeFactors.push(primesL[index]);
-	}
-	return [primeFactors.toString(), primeFactors.length];
-    }
     
-    for(var i=1; i<limit; i++)
+    for (var i=1; i<limit; i++)
 	divisors[i] = getPrimeFactors(i);
 
     var a = divisors[0],
         b = divisors[1], 
 	c = divisors[2],
         d = divisors[3];    
-    for(var i=4; i<limit && !number; i++) {
+
+    for (i=4; i<limit && !number; i++) {
 	a = b;
 	b = c;
 	c = d;
 	d = divisors[i];
-	if(a[1]==4 && b[1]==4 && c[1]==4 && d[1]==4 && a[0]!=b[0] && a[0]!=c[0] && a[0]!=d[0] && b[0]!=c[0] && b[0]!=d[0] && c[0]!=d[0])
-	    number = i-3;
+
+	if (a[1] == 4 && b[1] == 4 && c[1] == 4 && d[1] == 4 
+	    && a[0] != b[0] && a[0] != c[0] && a[0] != d[0] 
+	    && b[0] != c[0] && b[0] != d[0] 
+	    && c[0] != d[0])
+
+	    number = i - 3;
     }
 
     console.log("number: " + number);
--- Problem048.js
+++ Problem048.js
@@ -1,26 +1,28 @@
 exports.run = function () {
-    var sum = 0;
+    var sum = 0,
+	getSelfPower = function (number) {
+	    return raiseNumber(number, number);
+	},
+	raiseNumber = function (number, power) {
+	    for (var i=0, raisedNumber=1; i<power; i++)
+		raisedNumber = reduceToTenDigits(raisedNumber * number);
 
-    function getSelfPower(number) {
-	return raiseNumber(number, number);
-    }
+	    return raisedNumber;
+	},
+	reduceToTenDigits = function (number) {
+	    var stringNumber = number.toString();
 
-    function raiseNumber(number, power) {
-	var raisedNumber;
-	for(var i=0, raisedNumber=1; i<power; i++)
-	    raisedNumber = reduceToTenDigits(raisedNumber * number);
-	return raisedNumber;
-    };
+	    return stringNumber.length>10 
+		? +(stringNumber.substr(stringNumber.length-10)) 
+		: number;
+	},
+	output;
 
-    function reduceToTenDigits(number) {
-	var stringNumber = number.toString();
-	return stringNumber.length>10 ? parseInt(stringNumber.substr(stringNumber.length-10), 10) : number;
-    }
-
-    for(var i=1; i<=1000; i++)
+    for (var i=1; i<=1000; i++)
 	sum = reduceToTenDigits(sum) + getSelfPower(i);
 
-    var output = reduceToTenDigits(sum);
+    output = reduceToTenDigits(sum);
+
     console.log("output: " + output);
     return output;
-}
+};
