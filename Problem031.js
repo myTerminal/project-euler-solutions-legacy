64487842dba5c9652ae284c536785bac173be615
Ismail Ansari <ismail.ansari@citiustech.com>
2014-05-13 13:37:52 +0530

    Refactored problems 31, 32, 34 & 35

--- Problem031.js
+++ Problem031.js
@@ -1,24 +1,28 @@
 exports.run = function () {
     var denominations = [1, 2, 5, 10, 20, 50, 100, 200],
         limit = 200,
-    	count = 0;
+    	count = 0,
+	getCombinations = function (limit, denominations) {
+	    var count = 0,
+		currentCoin;
 
-    function getCombinations(limit, denominations) {
-	var count = 0,
-	    currentCoin;
+	    if (limit == 0)
+		return 1;
 
-	if(limit==0)
-	    return 1;
-	if(denominations.length<=0)
-	    return 0;
+	    if (denominations.length <= 0)
+		return 0;
 
-	currentCoin = denominations.pop();
-	for(var i=0; currentCoin*i<=limit; i++)
-	    count += getCombinations(limit-currentCoin*i, denominations.slice(0));
-	return count;
-    }
+	    currentCoin = denominations.pop();
+
+	    for (var i=0; (currentCoin*i)<=limit; i++)
+		count += getCombinations(limit-currentCoin*i,
+					 denominations.slice(0));
+
+	    return count;
+	};
 
     count = getCombinations(limit, denominations);
+
     console.log("count: " + count);
     return count;
 };
--- Problem034.js
+++ Problem034.js
@@ -4,22 +4,24 @@ exports.run = function () {
     var myFactorial = new common.Memoizer(common.factorialMemoizable),
         numbers = [],
         sumOfFactorialsOfDigits,
-        sum = 0;
+        sum = 0,
+	getSumOfFactorialsOfDigits = function (number) {
+	    var stringNumber = number.toString(),
+		sum = 0;
 
-    for(var i=10; i<1000000 ; i++) {
+	    for (var i=0; i<stringNumber.length; i++)
+		sum = sum +  myFactorial.getValue(+(stringNumber[i]));
+
+	    return sum;
+	};
+
+    for (var i=10; i<1000000; i++) {
 	sumOfFactorialsOfDigits = getSumOfFactorialsOfDigits(i);
-	if(sumOfFactorialsOfDigits==i) {
+
+	if (sumOfFactorialsOfDigits == i) {
 	    numbers.push(i);
 	    sum += i;
 	}
-    }
-
-    function getSumOfFactorialsOfDigits(number) {
-	var stringNumber = number.toString(),
-            sum = 0;
-	for(var i=0; i<stringNumber.length; i++)
-	    sum = sum +  myFactorial.getValue(parseInt(stringNumber[i]));
-	return sum;
     }
 
     console.log("sum: " + sum);
