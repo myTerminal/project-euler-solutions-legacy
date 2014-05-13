29f415360b9da75048ca2e0511f8f05702e15e52
Ismail Ansari <ismail.ansari@citiustech.com>
2014-05-13 17:07:00 +0530

    Refactored problems 63 & 67

--- Problem063.js
+++ Problem063.js
@@ -3,17 +3,19 @@ var common = require("./Common");
 exports.run = function () {
     var limit = 100,
         number,
+	hasExceeded,
         count = 0;
 
-    for(var digits=1, hasExceeded, skipCount=0; digits<limit; digits++, hasExceeded=false)
-	for(var i=1; !hasExceeded && skipCount<2; i++) {
+    for (var digits=1, skipCount=0; digits<limit; digits++, hasExceeded=false)
+	for (var i=1; !hasExceeded && skipCount<2; i++) {
 	    number = common.raiseBigNumber(i.toString(), digits);
 
-	    if(number.length == digits) {
+	    if (number.length == digits) {
 		count++;
 		skipCount = 0;
 	    }
-	    if(number.length > digits) {
+
+	    if (number.length > digits) {
 		hasExceeded = true;
 		skipCount++;
 	    }
