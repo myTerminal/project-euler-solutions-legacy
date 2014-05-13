6d903a08b36ce32cd4491823e53472eae81d90cd
Ismail Ansari <ismail.ansari@citiustech.com>
2014-05-13 16:15:34 +0530

    Refactored problems 52 & 55

--- Problem052.js
+++ Problem052.js
@@ -1,32 +1,35 @@
 exports.run = function () {
-    var number;
-
-    var number1,
+    var number,
+	number1,
         number2,
         number3,
         number4,
         number5,
-        number6;
+        number6,
+	scaleNumber = function (number, mag) {
+	    return number * mag;
+	},
+	getSortedDigits = function (number) {
+	    return number.toString().split("").sort().join("");
+	};
 
-    for(var i = 11; !number; i++) {
-	if(i.toString().length != scaleNumber(i, 6).toString().length)
+    for (var i = 11; !number; i++) {
+	if (i.toString().length != scaleNumber(i, 6).toString().length)
 	    continue;
+
 	number1 = getSortedDigits(i);
 	number2 = getSortedDigits(scaleNumber(i, 2));
 	number3 = getSortedDigits(scaleNumber(i, 3));
 	number4 = getSortedDigits(scaleNumber(i, 4));
 	number5 = getSortedDigits(scaleNumber(i, 5));
 	number6 = getSortedDigits(scaleNumber(i, 6));
-	if(number1 == number2 && number2 == number3 && number3 == number4 && number4 == number5 && number5 == number6)
+
+	if (number1 == number2 
+	   && number2 == number3 
+	   && number3 == number4 
+	   && number4 == number5 
+	   && number5 == number6)
 	    number = i;
-    }
-
-    function scaleNumber(number, mag) {
-	return number * mag;
-    }
-
-    function getSortedDigits(number) {
-	return number.toString().split("").sort().join("");
     }
     
     console.log("number: " + number);
--- Problem055.js
+++ Problem055.js
@@ -2,33 +2,32 @@ var common = require("./Common");
 
 exports.run = function () {
     var limit = 10000,
-        lychrelCount = 0;
+        lychrelCount = 0,
+	reverseNumber = function (number) {
+	    return number.split("").reverse().join("");
+	},
+	addReverse = function (number) {
+	    return common.addBigNumbers(number, reverseNumber(number));
+	},
+	isPalindrome = function (number) {
+	    return number == reverseNumber(number);
+	},
+	isLychrel = function (number) {
+	    var count = 0;
 
-    for(var i=1; i<limit; i++)
-	if(isLychrel(i.toString()))
+	    do {
+		number = addReverse(number);
+		count++;
+	    }
+	    while (!isPalindrome(number) 
+		   && count < 50);
+
+	    return count == 50;
+	};
+
+    for (var i=1; i<limit; i++)
+	if (isLychrel(i.toString()))
 	    lychrelCount++;
-
-    function reverseNumber(number) {
-	return number.split("").reverse().join("");
-    }
-
-    function addReverse(number) {
-	return common.addBigNumbers(number, reverseNumber(number));
-    }
-
-    function isPalindrome(number) {
-	return number == reverseNumber(number);
-    }
-
-    function isLychrel(number) {
-	var count = 0;
-	do {
-	    number = addReverse(number);
-	    count++;
-	}
-	while(!isPalindrome(number) && count<50);
-	return count==50;
-    }
 
     console.log("lychrelCount: " + lychrelCount);
     return lychrelCount;
