21acfe7b150c19ff20579d4d0b7dcbcb8f1715d9
myTerminal <team.terminal@gmail.com>
2014-09-25 22:48:42 +0530

    Resolved CodeClimate warnings in Problem041.js Problem059.js Problem089.js Problem089Tests.js Problem096.js [ci skip]

--- Solutions/Problem041.js
+++ Solutions/Problem041.js
@@ -2,78 +2,78 @@ exports.run = function () {
     var lastPanDigitalSequence,
         panDigitals,
         number,
-	getLastPanDigitalNumber = function (numberOfDigits) {
-	    for (var i=numberOfDigits, stringNumber=""; i>0; i--)
-		stringNumber = stringNumber + i.toString();
+        getLastPanDigitalNumber = function (numberOfDigits) {
+            for (var i=numberOfDigits, stringNumber=""; i>0; i--)
+                stringNumber = stringNumber + i.toString();
 
-	    return stringNumber;
-	},
-	generatePanDigitalSeries = function (word) {
-	    if (word.length === 1)
-		return [word];
-	    
-	    var list = [],
-		startLetter,
-		remainingWord,
-		smallGeorge;
+            return stringNumber;
+        },
+        generatePanDigitalSeries = function (word) {
+            if (word.length === 1)
+                return [word];
+            
+            var list = [],
+                startLetter,
+                remainingWord,
+                smallGeorge;
 
-	    for (var i=0; i<word.length; i++) {
-		startLetter = word[i];
-		remainingWord = word.replace(startLetter, "");
-		smallGeorge = generatePanDigitalSeries(remainingWord);
+            for (var i=0; i<word.length; i++) {
+                startLetter = word[i];
+                remainingWord = word.replace(startLetter, "");
+                smallGeorge = generatePanDigitalSeries(remainingWord);
 
-		for (var j=0; j<smallGeorge.length; j++)
-		    list.push(startLetter + smallGeorge[j]);
-	    }
+                for (var j=0; j<smallGeorge.length; j++)
+                    list.push(startLetter + smallGeorge[j]);
+            }
 
-	    return list;
-	},
-	isPandigitalSeriesComposite = function (sequence) {
-	    return isDivisibleByThree(sequence);
-	},
-	isSuspectedComposite = function (number) {
-	    var endsWithSuspicious = function (number) {
-		var list = [0, 2, 4, 5, 6, 8],
-	            isSuspicious,
-	            stringNumber = number.toString(),
-	            lastDigit = stringNumber.substr(stringNumber.length-1);
+            return list;
+        },
+        isPandigitalSeriesComposite = function (sequence) {
+            return isDivisibleByThree(sequence);
+        },
+        isSuspectedComposite = function (number) {
+            var endsWithSuspicious = function (number) {
+                var list = [0, 2, 4, 5, 6, 8],
+                    isSuspicious,
+                    stringNumber = number.toString(),
+                    lastDigit = stringNumber.substr(stringNumber.length-1);
 
-		for (var i=0; i<list.length && !isSuspicious; i++)
-		    isSuspicious = lastDigit==list[i];
+                for (var i=0; i<list.length && !isSuspicious; i++)
+                    isSuspicious = lastDigit==list[i];
 
-		return isSuspicious;
-	    };
-	    
-	    return endsWithSuspicious(number) || isDivisibleByThree(number);
-	},
-	isDivisibleByThree = function (number) {
-	    var stringNumber = number.toString(),
-		sum = 0;
+                return isSuspicious;
+            };
+            
+            return endsWithSuspicious(number) || isDivisibleByThree(number);
+        },
+        isDivisibleByThree = function (number) {
+            var stringNumber = number.toString(),
+                sum = 0;
 
-	    for (var i=0; i<stringNumber.length; i++)
-		sum += +(stringNumber[i]);
+            for (var i=0; i<stringNumber.length; i++)
+                sum += +(stringNumber[i]);
 
-	    return sum%3 == 0;
-	},
-	isPrime = function (number) {
-	    for (var i=2, isComposite; i<number/2 && !isComposite; i++)
-		isComposite = (number%i==0);
+            return sum%3 == 0;
+        },
+        isPrime = function (number) {
+            for (var i=2, isComposite; i<number/2 && !isComposite; i++)
+                isComposite = (number%i==0);
 
-	    return !isComposite;
-	};
+            return !isComposite;
+        };
 
     for (var i=9; i>1 && !number; i--) {
-	lastPanDigitalSequence = getLastPanDigitalNumber(i);
+        lastPanDigitalSequence = getLastPanDigitalNumber(i);
 
-	if (!isPandigitalSeriesComposite(lastPanDigitalSequence)) {
-	    panDigitals = generatePanDigitalSeries(lastPanDigitalSequence);
-	    number=0;
+        if (!isPandigitalSeriesComposite(lastPanDigitalSequence)) {
+            panDigitals = generatePanDigitalSeries(lastPanDigitalSequence);
+            number=0;
 
-	    for (i=0; i<panDigitals.length && !number; i++)
-		if (!isSuspectedComposite(panDigitals[i]) 
-		    && isPrime(panDigitals[i]))
-		    number = panDigitals[i];
-	}
+            for (i=0; i<panDigitals.length && !number; i++)
+                if (!isSuspectedComposite(panDigitals[i]) 
+                    && isPrime(panDigitals[i]))
+                    number = panDigitals[i];
+        }
     }
     
     console.log("largest prime: " + number);
--- Solutions/Problem089Tests.js
+++ Solutions/Problem089Tests.js
@@ -5,51 +5,51 @@ describe('Roman to Decimal conversion', function (){
     var convertor;
 
     before(function () {
-	convertor = solution.RomanConvertor;
+        convertor = solution.RomanConvertor;
     });
 
     it("I should be treated as 1", function () {
-	assert.equal(1, convertor.toDecimal("I"));
+        assert.equal(1, convertor.toDecimal("I"));
     });
 
     it("V should be treated as 5", function () {
-	assert.equal(5, convertor.toDecimal("V"));
+        assert.equal(5, convertor.toDecimal("V"));
     });
 
     it("X should be treated as 10", function () {
-	assert.equal(10, convertor.toDecimal("X"));
+        assert.equal(10, convertor.toDecimal("X"));
     });
 
     it("L should be treated as 50", function () {
-	assert.equal(50, convertor.toDecimal("L"));
+        assert.equal(50, convertor.toDecimal("L"));
     });
 
     it("C should be treated as 100", function () {
-	assert.equal(100, convertor.toDecimal("C"));
+        assert.equal(100, convertor.toDecimal("C"));
     });
 
     it("D should be treated as 500", function () {
-	assert.equal(500, convertor.toDecimal("D"));
+        assert.equal(500, convertor.toDecimal("D"));
     });
 
     it("M should be treated as 1000", function () {
-	assert.equal(1000, convertor.toDecimal("M"));
+        assert.equal(1000, convertor.toDecimal("M"));
     });
 
     it("descending numbers should be added", function () {
-	assert.equal(12, convertor.toDecimal("XII"));
+        assert.equal(12, convertor.toDecimal("XII"));
     });
 
     it("should subtract when in ascending order", function () {
-	assert.equal(4, convertor.toDecimal("IV"));
+        assert.equal(4, convertor.toDecimal("IV"));
     });
 
     it("should subtract smaller numeral from following digit", function () {
-	assert.equal(19, convertor.toDecimal("XIX"));
+        assert.equal(19, convertor.toDecimal("XIX"));
     });
 
     it("should treat subtractive pairs all over the number", function () {
-	assert.equal(49, convertor.toDecimal("XLIX"));
+        assert.equal(49, convertor.toDecimal("XLIX"));
     });
 });
 
@@ -57,47 +57,47 @@ describe('Decimal to Roman conversion', function (){
     var convertor;
 
     before(function () {
-	convertor = solution.RomanConvertor;
+        convertor = solution.RomanConvertor;
     });
 
     it("should consider 1 as I", function () {
-	assert.equal("I", convertor.toRoman(1));
+        assert.equal("I", convertor.toRoman(1));
     });
 
     it("should consider 2 as II", function () {
-	assert.equal("II", convertor.toRoman(2));
+        assert.equal("II", convertor.toRoman(2));
     });
 
     it("should consider 4 as IV", function () {
-	assert.equal("IV", convertor.toRoman(4));
+        assert.equal("IV", convertor.toRoman(4));
     });
 
     it("should consider 9 as IX", function () {
-	assert.equal("IX", convertor.toRoman(9));
+        assert.equal("IX", convertor.toRoman(9));
     });
 
     it("should consider 19 as XIX", function () {
-	assert.equal("XIX", convertor.toRoman(19));
+        assert.equal("XIX", convertor.toRoman(19));
     });
 
     it("should consider 49 as XLIX", function () {
-	assert.equal("XLIX", convertor.toRoman(49));
+        assert.equal("XLIX", convertor.toRoman(49));
     });
 
     it("should consider 100 as C", function () {
-	assert.equal("C", convertor.toRoman(100));
+        assert.equal("C", convertor.toRoman(100));
     });
 
     it("should consider 500 as D", function () {
-	assert.equal("D", convertor.toRoman(500));
+        assert.equal("D", convertor.toRoman(500));
     });
 
     it("should consider 1000 as M", function () {
-	assert.equal("M", convertor.toRoman(1000));
+        assert.equal("M", convertor.toRoman(1000));
     });
 
     it("should consider 1606 as MDCVI", function () {
-	assert.equal("MDCVI", convertor.toRoman(1606));
+        assert.equal("MDCVI", convertor.toRoman(1606));
     });
 });
 
@@ -105,27 +105,27 @@ describe('Optimizing Roman number representation', function () {
     var convertor;
 
     before(function () {
-	convertor = solution.RomanConvertor;
+        convertor = solution.RomanConvertor;
     });
 
     it("should convert IIII to IV", function () {
-	assert.equal("IV", convertor.optimizeRoman("IIII"));
+        assert.equal("IV", convertor.optimizeRoman("IIII"));
     });
 
     it("should convert XIIIIII to XVI", function () {
-	assert.equal("XVI", convertor.optimizeRoman("XIIIIII"));
+        assert.equal("XVI", convertor.optimizeRoman("XIIIIII"));
     });
 
     it("should convert XVIV to XIX", function () {
-	assert.equal("XIX", convertor.optimizeRoman("XVIV"));
+        assert.equal("XIX", convertor.optimizeRoman("XVIV"));
     });
 
     it("should convert XXXXVIIII to XLIX", function () {
-	assert.equal("XLIX", convertor.optimizeRoman("XXXXVIIII"));
+        assert.equal("XLIX", convertor.optimizeRoman("XXXXVIIII"));
     });
 
     it("should convert MCCCCCCVI to MDCVI", function () {
-	assert.equal("MDCVI", convertor.optimizeRoman("MCCCCCCVI"));
+        assert.equal("MDCVI", convertor.optimizeRoman("MCCCCCCVI"));
     });
 });
 
@@ -133,14 +133,14 @@ describe('Calculating number of characters that can be saved', function () {
     var convertor;
 
     before(function () {
-	convertor = solution.RomanConvertor;
+        convertor = solution.RomanConvertor;
     });
 
     it("should return 0 when supplied with I", function () {
-	assert.equal(0, convertor.calculateExtraCharacters("I"));
+        assert.equal(0, convertor.calculateExtraCharacters("I"));
     });
 
     it("should return 2 when supplied with IIII", function () {
-	assert.equal(2, convertor.calculateExtraCharacters("IIII"));
+        assert.equal(2, convertor.calculateExtraCharacters("IIII"));
     });
 });
